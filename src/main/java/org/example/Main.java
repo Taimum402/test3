504c8988b6b90050619ccb63eff2283851360463
Taimum402 <mdtaimum03@gmail.com>
2025-07-03 14:04:54 +0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "T:\\apps\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://tinyurl.com/");
+        // driver.manage().window().maximize();
+        // driver.navigate().to("https://tinyurl.com/app/register");
+        // driver.findElement(By.xpath("//*[@id=\"signup_name\"]")).sendKeys("Taimum");
+        // driver.findElement(By.xpath("//*[@id=\"signup_email\"]")).sendKeys("taimum08@gmail.com");
+        // driver.findElement(By.xpath("//*[@id=\"signup_password\"]")).sendKeys("Islam420");
+        //driver.findElement(By.xpath("//*[@id=\"input-telephone\"]")).sendKeys("01321010110");
+        // driver.findElement(By.xpath("//*[@id=\"signup_signup_btn\"]/span")).click();
+
+
+
+        Thread.sleep(10000);
+
+    }
+}
